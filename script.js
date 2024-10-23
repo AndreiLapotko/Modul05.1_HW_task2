fb9579b342a09e5f8d75ce15c0a7fe0e9e4987bd
AndreiLapotko <shudyx@gmail.com>
2024-10-23 18:31:59 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let name = JSON.parse(localStorage.getItem("name"));
+  displayName(name);
+});
+
+const button = document.querySelector("button");
+const inputName = document.getElementById("nameInput");
+
+button.addEventListener("click", () => {
+  const inputName = document.getElementById("nameInput");
+  let name = inputName.value.trim();
+  saveName(name);
+  displayName(name);
+  inputName.value = "";
+});
+
+function displayName(name) {
+    const output = document.getElementById("output");
+    if (name) {
+      output.innerHTML = `<p>${name}</p>`;
+    } else {
+      output.innerHTML = "<p>Укажите свое имя</p>";
+    }
+}
+
+
+function saveName(name) {
+  localStorage.setItem("name", JSON.stringify(name));
+}
